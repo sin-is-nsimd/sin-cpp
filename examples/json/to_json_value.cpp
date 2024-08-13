82d0466c1a0e138e44f3550f9fb4336081d623a1
Lénaïc BAGNÈRES <lenaicb@singularity.fr>
2024-08-13 22:50:34 +0200

    feat(json): Add `sincpp::to_json_value` function for 1D containers

--- examples/json/to_json_value.cpp
+++ examples/json/to_json_value.cpp
@@ -2,10 +2,33 @@
 
 #include <iostream>
 
+template <class C> void print(C const &c) {
+  for (auto const &e : c) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
+}
+
 int main() {
   std::cout << sincpp::to_json_value(3.14f).to_json() << std::endl; // 3.14
   std::cout << sincpp::to_json_value('c') << std::endl;             // c
   std::cout << sincpp::to_json_value("A string") << std::endl;      // A string
+
+  int const c_array_i[] = {1, 2, 3};
+  char const c_array_s[] = {'a', 'b', 'c'};
+  std::array<int, 3> const array_i = {21, 42, 73};
+  std::array<std::string, 2> const array_s = {"An", "array"};
+  print(sincpp::to_json_value(c_array_i)); // 1 2 3
+  print(sincpp::to_json_value(c_array_s)); // a b c
+  print(sincpp::to_json_value(array_i));   // 21 42 73
+  print(sincpp::to_json_value(array_s));   // An array
+
+  std::vector<char const *> const vector = {"A", "std::vector"};
+  std::list<std::string_view> const list = {"A", "std::list"};
+  std::set<std::string> const set = {"A", "std::set"};
+  print(sincpp::to_json_value(vector)); // A std::vector
+  print(sincpp::to_json_value(list));   // A std::list
+  print(sincpp::to_json_value(set));    // A std::set
 
   return 0;
 }
--- include/sincpp/json/to_json_value.hpp
+++ include/sincpp/json/to_json_value.hpp
@@ -23,8 +23,12 @@
 
 #include "../str/conv.hpp"
 #include "../type_traits/is_char.hpp"
+#include "../type_traits/is_container_1d.hpp"
 #include "../type_traits/is_string.hpp"
 
+#include <algorithm>
+#include <iostream>
+#include <ranges>
 #include <string_view>
 #include <type_traits>
 
@@ -70,6 +74,13 @@ public:
   /// @brief Get the JSON representation.
   /// @returns the JSON representation.
   std::string_view to_json() const { return {m_str.data(), m_size}; }
+
+  /// @brief Operator `<<` between a `std::ostream` and a number.
+  /// @param o A `std::ostream`.
+  /// @param n A number.
+  friend std::ostream &operator<<(std::ostream &o, number_str_t const &n) {
+    return o << n.to_json();
+  }
 };
 
 /**
@@ -133,6 +144,168 @@ template <class T>
   requires(std::is_arithmetic_v<T> && is_char_v<T> == false)
 number_str_t<sincpp::to_chars_max_size<T>()> to_json_value(T const t) {
   return number_str_t<sincpp::to_chars_max_size<T>()>(t);
+}
+
+// to_json_value: array
+
+/**
+ * @brief Get the JSON value of an array of strings.
+ *
+ * @tparam T String type.
+ * @tparam N Array size.
+ * @tparam A Array of strings type.
+ *
+ * @param a An array of strings.
+ *
+ * @returns the JSON value of the array of strings.
+ */
+template <class T, size_t N, class A>
+  requires(is_string_v<T> || is_char_v<T>)
+A const &array_to_json_value(A const &a) {
+  return a;
+}
+
+/**
+ * @brief Get the JSON value of an array of numbers.
+ *
+ * @tparam T String type.
+ * @tparam N Array size.
+ * @tparam A Array of numbers type.
+ *
+ * @param a An array of numbers.
+ *
+ * @returns the JSON value of the array of numbers.
+ */
+template <class T, size_t N, class A>
+  requires(std::is_arithmetic_v<T>)
+std::array<number_str_t<to_chars_max_size<T>()>, N>
+array_to_json_value(A const &a) {
+  std::array<number_str_t<to_chars_max_size<T>()>, N> r;
+  std::ranges::transform(a, r.begin(),
+                         [](T const v) { return make_number_str(v); });
+  return r;
+}
+
+/**
+ * @brief Get the JSON value of an array.
+ *
+ * @tparam T String type.
+ * @tparam N Array size.
+ *
+ * @param a An array.
+ *
+ * @returns the JSON value of the array.
+ *
+ * **Example:**
+ * @include examples/json/to_json_value.cpp
+ */
+template <class T, size_t N>
+auto to_json_value(std::array<T, N> const &a)
+    -> decltype(array_to_json_value<T, N>(a)) {
+  return array_to_json_value<T, N>(a);
+}
+
+// to_json_value: C array
+
+/**
+ * @brief Get the JSON value of a C array of numbers.
+ *
+ * @tparam T Number type.
+ * @tparam N Array size.
+ *
+ * @param a A C array of numbers.
+ *
+ * @returns the JSON value of the C array of numbers.
+ *
+ * **Example:**
+ * @include examples/json/to_json_value.cpp
+ */
+template <class T, size_t N>
+  requires(std::is_arithmetic_v<T> && std::is_same_v<T, char> == false)
+std::array<number_str_t<to_chars_max_size<T>()>, N>
+to_json_value(T const (&a)[N]) {
+  std::array<number_str_t<to_chars_max_size<T>()>, N> r;
+  std::transform(a, a + N, r.begin(),
+                 [](T const v) { return make_number_str(v); });
+  return r;
+}
+
+/**
+ * @brief Get the JSON value of a C array of strings.
+ *
+ * @tparam T String type.
+ * @tparam N Array size.
+ *
+ * @param a A C array of strings.
+ *
+ * @returns the JSON value of the C array of strings.
+ *
+ * **Example:**
+ * @include examples/json/to_json_value.cpp
+ */
+template <class T, int N>
+  requires(is_string_v<T>)
+std::array<std::string_view, N> to_json_value(T const (&a)[N]) {
+  std::array<std::string_view, N> r;
+  std::ranges::copy(a, a + N, r.begin());
+  return r;
+}
+
+// to_json_value: 1D container
+
+/**
+ * @brief Get the JSON value of an 1D container of strings.
+ *
+ * @tparam T String type.
+ * @tparam C 1D container of strings type.
+ *
+ * @param c An 1D container of strings.
+ *
+ * @returns the JSON value of the 1D container of strings.
+ */
+template <class T, class C>
+  requires(is_string_v<T> || is_char_v<T>)
+C const &container_1d_to_json_value(C const &c) {
+  return c;
+}
+
+/**
+ * @brief Get the JSON value of an 1D container of numbers.
+ *
+ * @tparam T Number type.
+ * @tparam C 1D container of numbers type.
+ *
+ * @param c An 1D container of numbers.
+ *
+ * @returns the JSON value of the 1D container of numbers.
+ */
+template <class T, class C>
+  requires(std::is_arithmetic_v<T>)
+std::vector<number_str_t<to_chars_max_size<T>()>>
+container_1d_to_json_value(C const &c) {
+  std::vector<number_str_t<to_chars_max_size<T>()>> r(c.size());
+  std::ranges::transform(c, r.begin(),
+                         [](T const v) { return make_number_str(v); });
+  return r;
+}
+
+/**
+ * @brief Get the JSON value of an 1D container.
+ *
+ * @tparam C 1D container type.
+ *
+ * @param c An 1D container.
+ *
+ * @returns the JSON value of the 1D container.
+ *
+ * **Example:**
+ * @include examples/json/to_json_value.cpp
+ */
+template <class C>
+  requires(is_container_1d_v<C>)
+auto to_json_value(C const &c)
+    -> decltype(container_1d_to_json_value<typename C::value_type>(c)) {
+  return container_1d_to_json_value<typename C::value_type>(c);
 }
 
 } // namespace sincpp
